3919940311912e54f10b76d0f031a93f08ca0930
Anwesh43 <anweshthecool0@gmail.com>
2019-09-21 11:12:32 +0530

    adding ScaleUtil class

--- ArcNeighborAnimatorStage.ts
+++ ArcNeighborAnimatorStage.ts
@@ -8,6 +8,17 @@ const foreColor : string = "#673AB7"
 const w : number = window.innerWidth
 const h : number = window.innerHeight
 
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+}
+
 class ArcNeighborAnimatorStage {
 
     context : CanvasRenderingContext2D
